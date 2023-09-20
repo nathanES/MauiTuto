3771991e50eed725dd1910d8453e525134b38216
nathan <nathan.esslinger67@gmail.com>
2023-09-20 07:44:03 +0200

    first commit

---
+++ MauiTuto/MauiTuto/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiTuto;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ MauiTuto/MauiTuto/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiTuto;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
